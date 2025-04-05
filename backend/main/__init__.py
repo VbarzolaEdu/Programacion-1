617a65fda2cd31a66bb9c49d3957101de813975b
valentinbarzola <v.barzola@alumno.um.edu.ar>
2025-04-05 16:42:48 -0300

    fixing bugs

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -5,10 +5,7 @@ from flask_restful import Api
 
 import main.resources as resources
 
-<<<<<<< HEAD
-=======
 
->>>>>>> franco
 #Inicializamos restful
 api = Api()
 
@@ -19,18 +16,15 @@ def create_app():
     load_dotenv()
        
     #cargar los recursos
-<<<<<<< HEAD
     api.add_resource(resources.UserResource, '/user/<id>')
     api.add_resource(resources.UsersResource, '/users')
     api.add_resource(resources.PedidoResource, '/pedido/<id>')
     api.add_resource(resources.PedidosResource, '/pedidos')
-=======
     
     api.add_resource(resources.Notificaciones, '/Notificaciones')
 
     api.add_resource(resources.Valoraciones, '/valoraciones')
-    api.add_resource(resources.Valoracion, '/valoraciones/<int:id>')
->>>>>>> franco
+    api.add_resource(resources.Valoracion, '/valoraciones/<id>')
     
     api.init_app(app)
     return app
--- backend/main/resources/__init__.py
+++ backend/main/resources/__init__.py
@@ -2,8 +2,6 @@ from .user import User as UserResource
 from .user import Users as UsersResource
 from .pedidos import Pedidos as PedidosResource
 from .pedidos import Pedido as PedidoResource
-from .animal import Animal as AnimalResource
-from .animal import Animales as AnimalesResource
 from .notificacioness import Notificaciones as Notificaciones
 from .valoracioness import Valoracion as Valoracion
 from .valoracioness import Valoraciones as Valoraciones
