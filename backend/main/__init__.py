52bbab50086cd128388b8a25761e3916b04e49cc
sanztincho <martinraulsanz@gmail.com>
2025-05-04 20:27:21 -0300

    trying to fix boot

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -19,10 +19,12 @@ def create_app():
     #cargamos variables de entorno
     load_dotenv()
     
+    #Si no existe el archivo de base de datos crearlo (solo válido si se utiliza SQLite)
     if not os.path.exists(os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME')):
         os.mknod(os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME'))
-    
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    #Url de configuración de base de datos
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME')
     db.init_app(app)
     
